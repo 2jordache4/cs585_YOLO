4ffabafa45247702f12d91af404777e7f77e6285
Anthony <111718236+Coeus137@users.noreply.github.com>
2025-04-29 20:21:47 -0400

    added car class, tested motor control, ultrasonic untested

---
+++ python_src/test_car.py
@@ -0,0 +1,281 @@
+import cv2
+import onnxruntime as ort
+import numpy as np
+import time
+import serial
+
+device_is_pi = False
+
+if device_is_pi:
+    from picamera2 import Picamera2
+    
+    def init_camera():
+        cap = Picamera2()
+        config = cap.create_preview_configuration({"format":"RGB888", "size":(320, 320)})
+        cap.configure(config)
+        cap.start()
+    
+        return cap
+
+    def close_camera(cap):
+        cap.stop()
+
+    def get_frame(cap):
+        frame = cap.capture_array()
+        
+        in_img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) # might be able to remove
+        in_img = in_img.astype(np.float32) / 255.0
+        in_img = np.transpose(in_img, (2, 0, 1))  # HWC → CHW
+        in_img = np.expand_dims(in_img, axis=0)  # Add batch dim
+        
+        return frame, in_img
+        
+else:
+    def init_camera():
+        cap = cv2.VideoCapture(0)
+    
+        return cap
+
+    def close_camera(cap):
+        cap.stop()
+
+    def get_frame(cap):
+        ret, frame = cap.read()
+        
+        if not ret:
+            quit()
+        
+        frame = cv2.resize(frame, (320, 320))
+        
+        in_img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) # might be able to remove
+        in_img = in_img.astype(np.float32) / 255.0
+        in_img = np.transpose(in_img, (2, 0, 1))  # HWC → CHW
+        in_img = np.expand_dims(in_img, axis=0)  # Add batch dim
+        
+        return frame, in_img
+
+'''
+    https://raspi.tv/2013/how-to-use-interrupts-with-python-on-the-raspberry-pi-and-rpi-gpio-part-3
+    
+    https://stackoverflow.com/questions/48597852/raspberry-pi-interrupts-python-gpio-library
+        
+    
+'''
+class CarUnit:
+    def __init__(self, dev, ultra_pin=None):
+        self.dev = dev
+        self.ultra = ultra_pin
+        
+    def connect(self, baud=9600, timeout=1):
+        # open dev/file
+        try:
+            self.usb = serial.Serial(self.dev, baud, timeout=timeout)
+        except:
+            print("Failed to open serial connection, script stopping")
+            quit()           
+        
+        self.usb.reset_input_buffer()
+        
+        
+        
+    def release(self):
+        # close dev/file
+        pass
+    
+    def write_motors(self, r_motor=0, l_motor=0):
+        # encode motor values
+        signs = 0
+        
+        if r_motor < 0:
+            signs += 1
+            r_motor = -r_motor
+            
+        if l_motor < 0:
+            signs += 2
+            l_motor = -l_motor
+        
+        # send over serial
+        self.usb.write(signs.to_bytes()+r_motor.to_bytes()+l_motor.to_bytes()+b"\n")
+        
+    def set_ultra(self, thresh):
+        # build config msg
+        #   upper nibble indicates which setting is changed,
+        #   lower nibble is ignored
+        config = (1 << 4) + 0
+        
+        # sent over serial
+        self.usb.write(config.to_bytes()+thresh.to_bytes()+b"\n")
+
+class SIFTdetect:
+    def __init__(self):
+        self.sift = cv2.SIFT_create()
+        self.matcher = cv2.BFMatcher()
+        
+        self.target = None
+        
+    def match_target(self, frame, bboxes):
+        # not boxes availible
+        if bboxes.shape[0] == 0:
+            return None
+        
+        # get best box index
+        best_idx = np.argmax(bboxes[:,4])
+        best_box = bboxes[best_idx, :]
+        
+        # crop the best box area
+        if best_box[0] < 0: best_box[0] = 0
+        if best_box[1] < 0: best_box[1] = 0
+        if best_box[2] > frame.shape[0]: best_box[2] = frame.shape[0]
+        if best_box[3] > frame.shape[0]: best_box[3] = frame.shape[0]
+        
+        best_crop = cv2.cvtColor(frame[best_box[1]:best_box[3], best_box[0]:best_box[2]], cv2.COLOR_BGR2GRAY)
+        
+        kp, des = self.sift.detectAndCompute(best_crop, None)
+        
+        if self.target is None:
+            self.target = des
+            
+            # save the first descriptors 
+            # I kinda want to make this like adapting so it can save features specific to a person
+            # rather than person + background but right now i think this is okay
+        
+        else:
+            matches = self.matcher.knnMatch(self.target, des, k=2)
+            
+            good = [] #[m.distance < 0.8 * n.distance for m, n in matches]
+            for m, n in matches: 
+                if m.distance < 0.8 * n.distance: 
+                    good.append([m])
+                  
+            if len(good) > 20:# kp threshhold, 10 was pretty consistent but im not convinced its enough
+                return best_idx
+
+def process_output(results, thresh, tol, bbox=False):
+    
+    results = np.squeeze(results)
+
+    if len(results.shape) == 1:
+        results = np.expand_dims(results, axis=0)
+    
+    # loops through each result, 
+    #   throws out any confidance below threshold,
+    #   and removes any duplicates
+    output_list = []
+    output = np.array(output_list)   
+    for result in results.T:
+        if result[4] < thresh: continue
+        
+        if output_list:
+            diff = (output + result) / (2*result)
+            
+            if np.all(diff > tol): continue
+            
+        output_list.append(result)
+        output = np.array(output_list)
+    
+    output = output.astype(int)
+    
+    if bbox:       
+        #  format: (center_x, center_y, w, h) -> (x1, y1, x2, y2)
+        
+        if output.shape[0] == 0:
+            return output, np.zeros((0,))
+
+        bbox_out = np.zeros(output.shape)
+
+        bbox_out[:,0] = output[:,0] - output[:,2]/2
+        bbox_out[:,1] = output[:,1] - output[:,3]/2
+        bbox_out[:,2] = output[:,0] + output[:,2]/2
+        bbox_out[:,3] = output[:,1] + output[:,3]/2
+        bbox_out[:,4] = output[:,4]
+        
+        bbox_out = bbox_out.astype(int)
+        
+        return output, bbox_out
+    
+    else:
+        return output
+
+def add_bboxes(frame, bboxes, target=None):
+    
+    if bboxes.shape[0] == 0:
+        return
+    
+    if len(bboxes.shape) == 1:
+        bboxes= np.expand_dims(bboxes, axis=0)
+    
+    for bbox in bboxes:
+        cv2.rectangle(frame, (bbox[0], bbox[1]), (bbox[2], bbox[3]), (0, 0, 255), 2)
+        
+    if target is not None:
+        x_val = int((bboxes[target, 0] + bboxes[target,2]) / 2)
+        cv2.line(frame, (x_val, 0), (x_val, frame.shape[0]), (255, 0, 0), 2)
+
+def preprocess(img):
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) # might be able to remove
+    img = img.astype(np.float32) / 255.0
+    img = np.transpose(img, (2, 0, 1))  # HWC → CHW
+    img = np.expand_dims(img, axis=0)  # Add batch dim
+    return img
+
+def format_opencv(x):
+    #  format: (center_x, center_y, w, h) -> (x1, y1, x2, y2)
+    y = x.copy()
+    y[0] = x[0] - x[2] / 2
+    y[1] = x[1] - x[3] / 2
+    y[2] = x[0] + x[2] / 2
+    y[3] = x[1] + x[3] / 2
+    
+    return map(int, y)
+
+print("Loading Model...")
+session = ort.InferenceSession("./best.onnx")
+input_name = session.get_inputs()[0].name
+
+print("Starting Capture...")
+cap = init_camera()
+
+sifter = SIFTdetect()
+
+car = CarUnit("/dev/ttyACM0")
+car.connect()
+'''
+print("Starting Loop...")
+print("\n")
+saved = 3
+while True:
+    start = time.time()
+    
+    # get camera image
+    frame, in_img = get_frame(cap)
+    
+    # run image through model
+    outputs = session.run(None, {input_name: in_img})[0]  
+    
+    # process model output
+    outputs, bboxes = process_output(outputs, 0.7, 0.8, bbox=True)
+    
+    # apply sift
+    target_idx = sifter.match_target(frame, bboxes)
+    
+    # put boxes on image
+    add_bboxes(frame, bboxes, target_idx)
+
+    end = time.time()
+    
+    if saved > 0 and outputs.shape[0] > 1:
+        cv2.imwrite(f"saved[{saved}].jpg", frame)
+        saved -= 1
+        
+    cv2.imshow("ONNX", frame)
+    
+    print(f"\033[A\r{1/(end-start):f} fps, {outputs.shape[0]} detected")
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+print("Stoping Capture...")
+close_camera()
+cv2.destroyAllWindows()
+'''
+print("Script Done")
